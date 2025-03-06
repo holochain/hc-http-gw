852c61f674303d0525599cffc7fe07f385fd21fc
Callum Dunster <cdunster@users.noreply.github.com>
2025-03-06 21:15:24 +0100

    feat: add check that app is in allowed app list

--- src/app_selection.rs
+++ src/app_selection.rs
@@ -2,19 +2,25 @@ use holochain_client::AppInfo;
 use holochain_types::dna::DnaHashB64;
 use thiserror::Error;
 
+use crate::config::AllowedAppIds;
+
 #[derive(Debug, PartialEq, Error)]
 pub enum AppSelectionError {
     #[error("App is not installed on the conductor")]
     NotInstalled,
+
+    #[error("App is not in the list of allowed apps")]
+    NotAllowed,
 }
 
 fn check_app_valid(
     dna_hash: DnaHashB64,
     installed_apps: &[AppInfo],
+    allowed_apps: &AllowedAppIds,
 ) -> Result<(), AppSelectionError> {
-    installed_apps
+    let app_info = installed_apps
         .iter()
-        .any(|a| {
+        .find(|a| {
             a.manifest.app_roles().iter().any(|r| {
                 r.dna
                     .installed_hash
@@ -22,13 +28,17 @@ fn check_app_valid(
                     .is_some_and(|hash| hash == &dna_hash)
             })
         })
+        .ok_or(AppSelectionError::NotInstalled)?;
+
+    allowed_apps
+        .contains(&app_info.installed_app_id)
         .then_some(())
-        .ok_or(AppSelectionError::NotInstalled)
+        .ok_or(AppSelectionError::NotAllowed)
 }
 
 #[cfg(test)]
 mod tests {
-    use std::collections::HashMap;
+    use std::{collections::HashMap, str::FromStr};
 
     use super::*;
     use assert2::assert;
@@ -64,18 +74,31 @@ mod tests {
     fn returns_error_if_app_not_installed() {
         let dna_hash = DnaHash::from_raw_32([1; 32].to_vec()).into();
         let installed_apps = [];
+        let allowed_apps = AllowedAppIds::from_str("").unwrap();
 
-        let result = check_app_valid(dna_hash, &installed_apps);
+        let result = check_app_valid(dna_hash, &installed_apps, &allowed_apps);
 
         assert!(result == Err(AppSelectionError::NotInstalled));
+    }
+
+    #[test]
+    fn returns_error_if_app_installed_but_not_allowed() {
+        let dna_hash: DnaHashB64 = DnaHash::from_raw_32([1; 32].to_vec()).into();
+        let installed_apps = [new_fake_app_info("some_app_id", dna_hash.clone())];
+        let allowed_apps = AllowedAppIds::from_str("other_app_id").unwrap();
+
+        let result = check_app_valid(dna_hash, &installed_apps, &allowed_apps);
+
+        assert!(result == Err(AppSelectionError::NotAllowed));
     }
 
     #[test]
     fn returns_ok_if_app_is_installed_and_allowed() {
         let dna_hash: DnaHashB64 = DnaHash::from_raw_32([1; 32].to_vec()).into();
         let installed_apps = [new_fake_app_info("some_app_id", dna_hash.clone())];
+        let allowed_apps = AllowedAppIds::from_str("some_app_id").unwrap();
 
-        let result = check_app_valid(dna_hash, &installed_apps);
+        let result = check_app_valid(dna_hash, &installed_apps, &allowed_apps);
 
         assert!(result == Ok(()));
     }
