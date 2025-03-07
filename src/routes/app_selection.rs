ee2fbffef1a476bf033fe87c2faa5079f420768d
Callum Dunster <cdunster@users.noreply.github.com>
2025-03-07 13:31:07 +0100

    feat: valid apps return their App ID

--- src/routes/app_selection.rs
+++ src/routes/app_selection.rs
@@ -1,19 +1,23 @@
-use axum::extract::{Path, State};
+use axum::{
+    extract::{Path, State},
+    Json,
+};
 use holochain_types::dna::DnaHashB64;
 
-use crate::{app_selection::check_app_valid, service::AppState, HcHttpGatewayResult};
+use crate::{app_selection::try_get_valid_app, service::AppState, HcHttpGatewayResult};
 
 #[tracing::instrument(skip(state))]
 pub async fn app_selection(
     Path(raw_dna_hash): Path<String>,
     State(mut state): State<AppState>,
-) -> HcHttpGatewayResult<()> {
+) -> HcHttpGatewayResult<Json<String>> {
     let dna_hash = DnaHashB64::from_b64_str(&raw_dna_hash)?;
-    check_app_valid(
+    let app_info = try_get_valid_app(
         dna_hash,
         &mut state.installed_apps,
         &state.configuration.allowed_app_ids,
         &state.admin_websocket,
-    )
-    .map_err(Into::into)
+    )?;
+
+    Ok(Json(app_info.installed_app_id))
 }
