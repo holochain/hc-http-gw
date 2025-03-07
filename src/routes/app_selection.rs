9e01a7de4b23412c20227fd727b26a0530c667e6
Callum Dunster <cdunster@users.noreply.github.com>
2025-03-07 13:17:22 +0100

    feat: check app_selection DNA hash is valid

--- src/routes/app_selection.rs
+++ src/routes/app_selection.rs
@@ -1,6 +1,34 @@
+use holochain_types::dna::DnaHashB64;
+
 use crate::HcHttpGatewayResult;
 
 #[tracing::instrument]
 pub async fn app_selection(raw_dna_hash: String) -> HcHttpGatewayResult<()> {
+    let _dna_hash = DnaHashB64::from_b64_str(&raw_dna_hash)?;
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::HcHttpGatewayError;
+
+    use super::*;
+    use assert2::assert;
+    use fixt::fixt;
+    use holochain_types::prelude::fixt::DnaHashB64Fixturator;
+
+    #[tokio::test]
+    async fn returns_hash_error_if_invalid_hash() {
+        let result = app_selection("invalid-hash".to_string()).await;
+
+        assert!(let Err(HcHttpGatewayError::HoloHashError(_)) = result);
+    }
+
+    #[tokio::test]
+    async fn returns_ok_if_valid_hash() {
+        let hash = fixt!(DnaHashB64);
+        let result = app_selection(hash.to_string()).await;
+
+        assert!(let Ok(()) = result);
+    }
 }
