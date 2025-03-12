b07cfcf5628245fb897ef6ac969fe0fd24971700
ThetaSinner <ThetaSinner@users.noreply.github.com>
2025-03-12 12:03:13 +0000

    chore: Move modules around

--- src/lib.rs
+++ src/lib.rs
@@ -2,20 +2,19 @@
 //! # Holochain HTTP gateway
 
 mod app_selection;
-mod cli;
-pub mod config;
+mod config;
 mod error;
 mod holochain;
 mod resolve;
 mod router;
 mod routes;
 mod service;
-pub mod transcode;
+mod transcode;
 
 #[cfg(any(test, feature = "test-utils"))]
 pub mod test_tracing;
 
-pub use cli::HcHttpGatewayArgs;
+pub use config::*;
 pub use error::{HcHttpGatewayError, HcHttpGatewayResult};
 pub use holochain::*;
 pub use resolve::resolve_address_from_url;
