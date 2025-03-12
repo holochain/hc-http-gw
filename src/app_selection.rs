25eb5003b9eaec3224def405841950d98a618e93
ThetaSinner <ThetaSinner@users.noreply.github.com>
2025-03-12 13:17:43 +0000

    chore: Downgrade lock

--- src/app_selection.rs
+++ src/app_selection.rs
@@ -64,6 +64,7 @@ pub async fn try_get_valid_app(
 
                 let mut installed_apps = installed_apps.write().await;
                 *installed_apps = new_installed_apps.clone();
+                let installed_apps = installed_apps.downgrade();
                 choose_unique_app(&dna_hash, &coordinator_identifier, &installed_apps)?.clone()
             } else {
                 // We either couldn't get a response from Holochain or the response was empty.
