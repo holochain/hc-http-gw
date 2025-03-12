d9c5a3626e38eadf9699ffd405efde1ab6580c6a
Jost <jost-s@users.noreply.github.com>
2025-03-11 20:53:48 -0600

    refactor(transcode)!: accept optional payload for better ergonomics

--- src/transcode.rs
+++ src/transcode.rs
@@ -8,16 +8,26 @@
 
 use crate::{HcHttpGatewayError, HcHttpGatewayResult};
 use base64::{prelude::BASE64_URL_SAFE, Engine};
+use holochain_client::ConductorApiError;
 use holochain_types::prelude::ExternIO;
 
-/// Function to transcode an incoming base64 encoded payload to Holochain serialized bytes
-/// (type `ExternIO`).
-pub fn base64_json_to_hsb(base64_encoded_payload: &str) -> HcHttpGatewayResult<ExternIO> {
-    let base64_decoded_payload = BASE64_URL_SAFE
-        .decode(base64_encoded_payload)
-        .map_err(|_| HcHttpGatewayError::RequestMalformed("Invalid base64 encoding".to_string()))?;
-    let json_payload = serde_json::from_slice::<serde_json::Value>(&base64_decoded_payload)
-        .map_err(|_| HcHttpGatewayError::RequestMalformed("Invalid JSON value".to_string()))?;
+/// Function to transcode an optional base64 encoded payload to Holochain serialized bytes
+/// (type `ExternIO`). If no payload is passed in, a unit value will be serialized.
+pub fn base64_json_to_hsb(
+    maybe_base64_encoded_payload: Option<String>,
+) -> HcHttpGatewayResult<ExternIO> {
+    let json_payload = if let Some(base64_encoded_payload) = maybe_base64_encoded_payload {
+        let base64_decoded_payload =
+            BASE64_URL_SAFE
+                .decode(base64_encoded_payload)
+                .map_err(|_| {
+                    HcHttpGatewayError::RequestMalformed("Invalid base64 encoding".to_string())
+                })?;
+        serde_json::from_slice::<serde_json::Value>(&base64_decoded_payload)
+            .map_err(|_| HcHttpGatewayError::RequestMalformed("Invalid JSON value".to_string()))?
+    } else {
+        serde_json::Value::Null
+    };
     let msgpack_encoded_payload = ExternIO::encode(json_payload).map_err(|err| {
         HcHttpGatewayError::RequestMalformed(format!("Failure to serialize payload - {err}"))
     })?;
@@ -26,8 +36,12 @@ pub fn base64_json_to_hsb(base64_encoded_payload: &str) -> HcHttpGatewayResult<E
 
 /// Function to transcode a zome call response encoded as Holochain serialized bytes (type `ExternIO`)
 /// to a JSON string.
-pub fn hsb_to_json(hsb_encoded_response: &ExternIO) -> anyhow::Result<String> {
-    let json_value = hsb_encoded_response.decode::<serde_json::Value>()?;
+pub fn hsb_to_json(hsb_encoded_response: &ExternIO) -> HcHttpGatewayResult<String> {
+    let json_value = hsb_encoded_response
+        .decode::<serde_json::Value>()
+        .map_err(|err| {
+            HcHttpGatewayError::HolochainError(ConductorApiError::WebsocketError(err.into()))
+        })?;
     Ok(json_value.to_string())
 }
 
@@ -43,6 +57,16 @@ mod tests {
     use serde::{Deserialize, Serialize};
 
     #[test]
+    fn happy_no_payload_encode() {
+        // No payload needs to be encoded for zome call invocation too. Test that a unit value is encoded.
+        let hsb_encoded_payload = base64_json_to_hsb(None).unwrap();
+
+        // Deserializing the serialized bytes to the original struct should succeed.
+        let decoded_payload = hsb_encoded_payload.decode::<()>().unwrap();
+        assert_eq!(decoded_payload, ());
+    }
+
+    #[test]
     fn happy_base64_json_to_hsb() {
         #[derive(Debug, PartialEq, Deserialize, Serialize)]
         struct ZomeCallPayload {
@@ -54,7 +78,7 @@ mod tests {
         let json_payload = serde_json::to_string(&payload).unwrap();
         let base64_encoded_payload = BASE64_URL_SAFE.encode(json_payload);
 
-        let hsb_encoded_payload = base64_json_to_hsb(&base64_encoded_payload).unwrap();
+        let hsb_encoded_payload = base64_json_to_hsb(Some(base64_encoded_payload)).unwrap();
 
         // Deserializing the serialized bytes to the original struct should succeed.
         let decoded_payload = hsb_encoded_payload.decode::<ZomeCallPayload>().unwrap();
@@ -71,7 +95,7 @@ mod tests {
         let payload = ZomeCallPayload { field: false };
         let json_payload = serde_json::to_string(&payload).unwrap();
 
-        let result = base64_json_to_hsb(&json_payload);
+        let result = base64_json_to_hsb(Some(json_payload));
         let_assert!(HcHttpGatewayError::RequestMalformed(err) = result.unwrap_err());
         assert_eq!(err.to_string(), "Invalid base64 encoding");
     }
@@ -80,7 +104,7 @@ mod tests {
     fn invalid_json_to_hsb_fails() {
         let base64_encoded_payload = BASE64_URL_SAFE.encode("invalid");
 
-        let result = base64_json_to_hsb(&base64_encoded_payload);
+        let result = base64_json_to_hsb(Some(base64_encoded_payload));
         let_assert!(HcHttpGatewayError::RequestMalformed(err) = result.unwrap_err());
         assert_eq!(err.to_string(), "Invalid JSON value");
     }
